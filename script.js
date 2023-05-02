f246d807de9a866a8d08f9d23536a7e58268314a
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-05-02 21:12:24 +0400

    Make sure that when you click on any of the links, the main area changes to display only one of the three content sections

--- script.js
+++ script.js
@@ -65,3 +65,25 @@ const booksCollection = new BooksCollection();
 
 booksCollection.init();
 booksCollection.displayBooks();
+
+const listButton = document.querySelector('.list');
+const addNewButton = document.querySelector('.add-new');
+const contactButton = document.querySelector('.contact');
+const awesomeBooksSection = document.querySelector('.awesome-books');
+const addNewSection = document.querySelector('.add-book-form');
+const contactInfo = document.querySelector('.contact-info');
+listButton.addEventListener('click', () => {
+  awesomeBooksSection.classList.add('show');
+  addNewSection.classList.add('hide');
+  contactInfo.classList.add('hide');
+});
+addNewButton.addEventListener('click', () => {
+  addNewSection.classList.remove('hide');
+  awesomeBooksSection.classList.remove('show');
+  contactInfo.classList.add('hide');
+});
+contactButton.addEventListener('click', () => {
+  contactInfo.classList.remove('hide');
+  awesomeBooksSection.classList.remove('show');
+  addNewSection.classList.add('hide');
+});
